1620fdbb194dfc0409b8227ddf26fe628995eec8
WalterNet0 <162221447+WalterNet0@users.noreply.github.com>
2025-02-17 18:29:12 -0300

    Create E1.cs

--- C#/Udemy/Estrutura Condicional/E1.cs
+++ C#/Udemy/Estrutura Condicional/E1.cs
@@ -1,1 +1,17 @@
+using System;
+using System.Globalization;
 
+namespace Course {
+    class Program {
+        public static void Main(string[] args) {
+            Console.WriteLine("Digite um número inteiro: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero % 2 == 0)
+                Console.WriteLine("O número digitado é par!");
+
+            else
+                Console.WriteLine("O número digitado é ímpar!");
+        }   
+    }
+}
