5760a2fbb9fc14260d721b4890d7f3a2e508a17a
WalterNet0 <162221447+WalterNet0@users.noreply.github.com>
2025-02-18 13:10:14 -0300

    Add files via upload

---
+++ C#/Udemy/Seção 4/Classes e Métodos/Retangulo.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+namespace Course;
+
+public class Retangulo {
+    public double altura, largura;
+    
+    public double Area() {
+        return altura * largura;
+    }
+
+    public double Perimetro() {
+        return 2 * (altura + largura);
+    }
+
+    public double Diagonal() {
+        return Math.Sqrt(Math.Pow(altura, 2) + Math.Pow(largura, 2));
+    }
+
+    public override string ToString() {
+        return "Área: " + Area().ToString("F2", CultureInfo.InvariantCulture)
+            +  "\nPerímetro: " + Perimetro().ToString("F2", CultureInfo.InvariantCulture)
+            +  "\nDiagonal: " + Diagonal().ToString("F2", CultureInfo.InvariantCulture);
+    }
+}
